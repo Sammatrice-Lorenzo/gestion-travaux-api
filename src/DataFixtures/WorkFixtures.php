34fb629d53076e6137a7362e978618c525edfa61
Sammatrice Lorenzo <lorenzosammatrice@gmail.com>
2023-04-14 14:36:59 +0200

    done fixtures work and add enum progression and remove node_module

--- src/DataFixtures/WorkFixtures.php
+++ src/DataFixtures/WorkFixtures.php
@@ -2,20 +2,30 @@
 
 namespace App\DataFixtures;
 
-use Doctrine\Bundle\FixturesBundle\Fixture;
+use DateTime;
+use DateInterval;
+use Faker\Factory;
+use App\Entity\Work;
+use App\Enum\Progression;
 use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
 
 class WorkFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
         $faker = Factory::create('fr_FR');
-        for ($i=0; $i < ; $i++) { 
-            new (Work())
+        for ($i=0; $i < 10; $i++) {
+            $progression = $faker->randomElements(Progression::cases())[0]->value;
+            $work = (new Work())
                 ->setCity($faker->city)
-                ->setStrart(new Datetime())
-                ->setEnd((new DateTime())->addDate('P30D'))
-                ->setEquipement()
+                ->setStart(new Datetime())
+                ->setEnd((new DateTime())->add(new DateInterval('P30D')))
+                ->setEquipement(["Serveur", "Cables"])
+                ->setProgression($progression)
+            ;
+
+            $manager->persist($work);
         }
     
         $manager->flush();
