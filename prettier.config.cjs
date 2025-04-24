ab1f851ec798c9848a6f35d92195d6c385700400
Sammatrice Lorenzo <lorenzosammatrice@gmail.com>
2025-04-24 23:32:17 +0200

    add a php cs-fixdr with prettier

---
+++ prettier.config.cjs
@@ -0,0 +1,15 @@
+/**
+ * @see https://prettier.io/docs/configuration
+ */
+module.exports = {
+    semi: false,
+    singleQuote: true,
+    trailingComma: 'es5',
+    tabWidth: 4,
+    useTabs: false,
+    endOfLine: 'lf',
+    printWidth: 120,
+    bracketSpacing: true,
+    bracketSameLine: false,
+  }
+  
